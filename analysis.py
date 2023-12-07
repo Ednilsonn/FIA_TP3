790fe012940e4155a4f4b13e93e31857e28fe779
Ednilsonn <erodigues@uta.cv>
2023-12-07 11:26:39 -0100

    Q3 implementado

--- analysis.py
+++ analysis.py
@@ -27,35 +27,35 @@ def question2():
 def question3a():
     answerDiscount = 0.1
     answerNoise = 0
-    answerLivingReward = 0.7
+    answerLivingReward = 0.5
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
 def question3b():
-    answerDiscount = 0.1
-    answerNoise = 0
-    answerLivingReward = 1
+    answerDiscount = 0.3
+    answerNoise = 0.2
+    answerLivingReward = 0.1
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
 def question3c():
     answerDiscount = 0.1
     answerNoise = 0
-    answerLivingReward = 1
+    answerLivingReward = 5
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
 def question3d():
-    answerDiscount = 0.1
-    answerNoise = 0
+    answerDiscount = 0.3
+    answerNoise = 0.2
     answerLivingReward = 1
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
 def question3e():
-    answerDiscount = 0.1
-    answerNoise = 0
-    answerLivingReward = 1
+    answerDiscount = 0.3
+    answerNoise = 0.2
+    answerLivingReward = 10
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
